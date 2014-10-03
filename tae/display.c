3ef2614544cfc97f97a92f01457e33b89f1a57c2
Martin Kelm <martinkelm@shrt.ws>
2014-10-03 11:13:53 +0200

    more input output dsp logic

--- tae/display.c
+++ tae/display.c
@@ -1,5 +1,7 @@
 #include <ncurses.h>
+#include <string.h>
 #include <locale.h>
+#include <math.h>
 
 #define DEBUG
 
@@ -34,7 +36,7 @@ void dsp_windows_init(void) {
   refresh();
 
   wbkgd(header, COLOR_PAIR(1));
-  mvwaddstr(header, 0, 1, "Area one, place tow");
+  mvwaddstr(header, 0, 1, "Area one, place two");
   mvwaddstr(header, 0, maxx-23, "Adventure demo, v0.0.1");
   wrefresh(header);
 
@@ -43,12 +45,38 @@ void dsp_windows_init(void) {
   wrefresh(footer);
 
   wbkgd(output, COLOR_PAIR(2));
-  mvwaddstr(output, 1, 1, "Something in the output window");
   wrefresh(output);
 
   wbkgd(input, COLOR_PAIR(5));
-  mvwaddstr(input, 0, 1, ">> ");
+  wattrset(input, A_BOLD);
   wrefresh(input);
+}
+
+char *dsp_input(void) {
+  static char str[25];
+  wclear(input);
+  mvwaddstr(input, 0, 1, ">> ");
+  curs_set(1);
+  mvwgetnstr(input, 0, 4, str, 24);
+  curs_set(0);
+  return str;
+}
+
+void dsp_output(char *str) {
+  char *t;
+  t = str;
+  int line = 0, len = strlen(str), llen = maxx-3;
+  line = (int)ceil(((double)len) / llen);
+  printf("%d", line);
+  while (len) {
+    if (len > 1 && len % llen == 0) {
+      line--;
+    }
+    wmove(output, line, 1);
+    winsch(output, *(t+len-1));
+    len--;
+  }
+  wrefresh(output);
 }
 
 #ifdef DEBUG
@@ -57,6 +85,10 @@ int main(void) {
   dsp_init();
 
   dsp_windows_init();
+
+  char output[256] = "Something in the output window, and something other than my dog in window fight. But in all situations it is the best to perform a rabbit. it is like any other game here and there. Motivations are good to have so we go further and further.";
+  dsp_output(output);
+  char *input = dsp_input();
 
   dsp_end();
   return 0;
