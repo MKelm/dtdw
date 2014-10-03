e0237ac373f2b59d504c3b277ed6856f7d8494f3
Martin Kelm <martinkelm@shrt.ws>
2014-10-03 19:08:46 +0200

    added id items and id verb logic to loader

--- tae/loader.c
+++ tae/loader.c
@@ -10,39 +10,78 @@
 
 struct description {
   int id;
+  int id_items[5];
+  char id_verb[24];
   char text[1024];
-  int transitions[5];
-  int items[10];
+  int transitions[5]; // text transitions
+  int items[10]; // text items
 };
 struct description desc_entries[1000];
 
+/*
+ Description definitions:
+ #placeId$itemId...(optional)/verb(optional)
+ Text with #transition# or $item$ ...
+ #transitionID...$itemID...
+*/
 int load_descriptions(void) {
   FILE *f = fopen(FILE_DESC, "r");
   int ch, entryidx = 0;
-  // load status values
-  int lroomid = 0, ltransid = 0, ltransididx = 0, litemsid = 0, litemsididx = 0;
+  // load status values: main id, id items id, id verb, transition id, items id
+  int loadid = 0, liditemsid = 0, lidverb = 0, ltransid = 0, litemsid = 0;
+  // transition / item ids indexes
+  int liditemidx = 0, ltransidx = 0, litemidx = 0;
+  // temporary characters
   char line[1024] = "", chstr[2];
-  // 0 = load room id, 1 = load text, 2 = load transitions and items
-  int lroom = 0;
+  // load modes: 0 = id (+ desc items) (+ verb), 1 = text, 2 = transitions and items
+  int loadmode = 0;
 
   while ((ch = fgetc(f)) != EOF) {
-    switch (lroom) {
+    switch (loadmode) {
       case 0:
-        // load room id
-        // todo: load room id with item / verb combination
-        if (ch == '#' && lroomid == 0) {
-          lroomid = 1;
-        } else if (lroomid == 1 && ch != '\n') {
+        // load desc id
+        if (ch == '#' && loadid == 0) {
+          loadid = 1;
+        } else if (loadid == 1 && ch != '\n' && ch != '$' && ch != '/') {
           snprintf(chstr, 2, "%c", ch);
           strcat(line, chstr);
-        } else if (lroomid == 1 && ch == '\n') {
-          lroomid = 0;
+        } else if (loadid == 1 && (ch == '\n' || ch == '$' || ch == '/')) {
+          loadid = 0;
           desc_entries[entryidx].id = atoi(line);
           printf("\nroom id %d:\n", desc_entries[entryidx].id);
           strncpy(line, "", sizeof(line));
-          ltransididx = 0;
-          litemsididx = 0;
-          lroom = 1;
+          ltransidx = 0;
+          litemidx = 0;
+          if (ch == '\n')
+            loadmode = 1;
+        }
+        // load id item ids
+        if (ch == '$' && liditemsid == 0) {
+          liditemsid = 1;
+        } else if (liditemsid == 1 && ch != '\n' && ch != '$' && ch != '/') {
+          snprintf(chstr, 2, "%c", ch);
+          strcat(line, chstr);
+        } else if (liditemsid == 1 && (ch == '\n' || ch == '$' || ch == '/')) {
+          desc_entries[entryidx].id_items[liditemidx] = atoi(line);
+          printf("id item %d = %s\n ", liditemidx, line);
+          strncpy(line, "", sizeof(line));
+          liditemidx++;
+          liditemsid = (ch == '$') ? 1 : 0;
+          if (ch == '\n')
+            loadmode = 1;
+        }
+        // load id verb ids
+        if (ch == '/' && lidverb == 0) {
+          lidverb = 1;
+        } else if (lidverb == 1 && ch != '\n') {
+          snprintf(chstr, 2, "%c", ch);
+          strcat(line, chstr);
+        } else if (lidverb == 1 && ch == '\n') {
+          strncpy(desc_entries[entryidx].id_verb, line, sizeof(desc_entries[entryidx].id_verb));
+          printf("id verb = %s\n ", desc_entries[entryidx].id_verb);
+          strncpy(line, "", sizeof(line));
+          lidverb = 0;
+          loadmode = 1;
         }
         break;
       case 1:
@@ -54,7 +93,7 @@ int load_descriptions(void) {
           strncpy(desc_entries[entryidx].text, line, sizeof(desc_entries[entryidx].text));
           printf("desc = %s\n ", desc_entries[entryidx].text);
           strncpy(line, "", sizeof(line));
-          lroom = (desc_entries[entryidx].id == 0) ? 0 : 2;
+          loadmode = (desc_entries[entryidx].id == 0) ? 0 : 2;
         }
         break;
       case 2:
@@ -65,10 +104,10 @@ int load_descriptions(void) {
           snprintf(chstr, 2, "%c", ch);
           strcat(line, chstr);
         } else if (ltransid == 1 && (ch == '\n' || ch == '#' || ch == '$')) {
-          desc_entries[entryidx].transitions[ltransididx] = atoi(line);
-          printf("trans %d = %d\n ", ltransididx, desc_entries[entryidx].transitions[ltransididx]);
+          desc_entries[entryidx].transitions[ltransidx] = atoi(line);
+          printf("trans %d = %d\n ", ltransidx, desc_entries[entryidx].transitions[ltransidx]);
           strncpy(line, "", sizeof(line));
-          ltransididx++;
+          ltransidx++;
           ltransid = (ch == '#') ? 1 : 0;
         }
         // load item ids
@@ -78,16 +117,16 @@ int load_descriptions(void) {
           snprintf(chstr, 2, "%c", ch);
           strcat(line, chstr);
         } else if (litemsid == 1 && (ch == '\n' || ch == '$')) {
-          desc_entries[entryidx].items[litemsididx] = atoi(line);
-          printf("item %d = %s\n ", litemsididx, line);
+          desc_entries[entryidx].items[litemidx] = atoi(line);
+          printf("item %d = %s\n ", litemidx, line);
           strncpy(line, "", sizeof(line));
-          litemsididx++;
+          litemidx++;
           litemsid = (ch == '$') ? 1 : 0;
         }
         // jump to next room description after transitions / items
         if (ch == '\n') {
           entryidx++;
-          lroom = 0;
+          loadmode = 0;
         }
         break;
     }
