a220de21aa62ea6e10bba5464ec952c45f05e162
Martin Kelm <martinkelm@shrt.ws>
2014-10-05 09:32:16 +0200

    removed deprecated comment

--- tae/display.c
+++ tae/display.c
@@ -69,7 +69,6 @@ void dsp_set_location(struct area *area_data, struct place *place_data) {
   wrefresh(header);
 }
 
-// todo: insert / wrap new input only to keep buffer size and to avoid crashes
 void dsp_set_output(char *str) {
   char buffer[2048];
   waddstr(output, dsp_word_wrap(buffer, str, maxx-2));
