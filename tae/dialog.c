d4161a0c86c64cc1df79da2b9dfd4addf0776820
Martin Kelm <martinkelm@shrt.ws>
2014-10-07 06:33:31 +0200

    dialog module improved next ids output

--- tae/dialog.c
+++ tae/dialog.c
@@ -13,50 +13,53 @@ void dialog_set_current(struct dialog *ptr_dialog) {
 }
 
 char *dialog_get_next_output(void) {
-  static char output[1024];
+  static char output[1024], line[1024];
   int has_next_ids = 0;
-  if (dialog_element_idx == 0) {
-    // first output, todo change logic to handle next ids directly
-    snprintf(output, 1024, "\"%s\"\n\n", c_dialog->elements[dialog_element_idx].text);
-    return output;
-  } else if (dialog_element_idx > 0 &&
-             c_dialog->elements[dialog_element_idx-1].next_mchoice == 1) {
-    // multiple choice output
-    is_multiple_choice = 1; // flag for input mode
-    has_next_ids = 1;
-  } else if (c_dialog->elements[dialog_element_idx-1].next_ids[0] > 0) {
-    // regular output
-    is_multiple_choice = 0;
-    has_next_ids = 1;
-  }
-  if (has_next_ids == 1) {
-    int i;
-    for (i = 0; i < DIALOG_ELEMENT_MAX_NEXT_IDS; i++) {
-      if (c_dialog->elements[dialog_element_idx-1].next_ids[i] > 0) {
+  strncpy(output, "", sizeof(output));
+  if (dialog_element_idx > -1) {
+    // first output
+    snprintf(output, 1024, "\"%s\"\n", c_dialog->elements[dialog_element_idx].text);
+    if (c_dialog->elements[dialog_element_idx].next_mchoice == 1) {
+      // multiple choice output
+      is_multiple_choice = 1; // flag for input mode
+      has_next_ids = 1;
+    } else if (c_dialog->elements[dialog_element_idx].next_ids[0] > 0) {
+      // regular output
+      is_multiple_choice = 0;
+      has_next_ids = 1;
+    }
+    if (has_next_ids == 1) {
+      int i;
+      for (i = 0; i < DIALOG_ELEMENT_MAX_NEXT_IDS; i++) {
+        if (c_dialog->elements[dialog_element_idx].next_ids[i] > 0) {
 
-        struct dialogelement *c_element = dialog_get_element(
-          c_dialog->elements[dialog_element_idx-1].next_ids[i]
-        );
-        if (is_multiple_choice == 1) {
-          snprintf(output, 1024, "*  \"%s\" (%d)\n", c_element->text, i+1);
-        } else {
-          snprintf(output, 1024, "\"%s\"\n", c_element->text);
+          dialog_element_idx = dialog_get_next_element_idx(
+            c_dialog->elements[dialog_element_idx].next_ids[i]
+          );
+          if (is_multiple_choice == 1) {
+            snprintf(line, 1024, "*  \"%s\" (%d)\n",
+              c_dialog->elements[dialog_element_idx].text, i+1);
+          } else {
+            snprintf(line, 1024, "\"%s\"\n",
+              c_dialog->elements[dialog_element_idx].text);
+          }
+          strcat(output, line);
         }
       }
+      strcat(output, "\n");
     }
-    strcat(output, "\n");
-    return output;
   }
-  return "";
+  return output;
 }
 
-struct dialogelement *dialog_get_element(int id) {
-  int i;
-  for (i = 0; i < DIALOG_MAX_ELEMENTS; i++) {
-    if (c_dialog->elements[i].id == id)
-      return &c_dialog->elements[i];
+int dialog_get_next_element_idx(int id) {
+  int idx;
+  for (idx = dialog_element_idx; idx < DIALOG_MAX_ELEMENTS; idx++) {
+    if (c_dialog->elements[idx].id == id) {
+      return idx;
+    }
   }
-  return NULL;
+  return dialog_element_idx;
 }
 
 void dialog_set_input(char *input) {
--- tae/dialog.h
+++ tae/dialog.h
@@ -4,6 +4,6 @@ void dialog_set_current(struct dialog *ptr_dialog);
 
 char *dialog_get_next_output(void);
 
-struct dialogelement *dialog_get_element(int id);
+int dialog_get_next_element_idx(int id);
 
 void dialog_set_input(char *input);
